550a4b7e499a1d08dd68b86c2adaa8190c64d8f1
krayzpipes <kylepiper29@gmail.com>
2019-05-19 19:11:24 -0400

    Added sanity_check function tests

--- tests/_sanity/test_sanity.py
+++ tests/_sanity/test_sanity.py
@@ -48,3 +48,39 @@ def test_luhn_for_value_error_with_delimeters():
     non_int_with_delims = "123-abc"
     with pytest.raises(ValueError) as e_info:
         _ = luhn(non_int_with_delims)
+
+
+@pytest.fixture(scope="module")
+def always_true_algorithm_stub():
+    def stub_func(not_used):
+        return True
+    return stub_func
+
+
+@pytest.fixture(scope="module")
+def always_false_algorithm_stub():
+    def stub_func(not_used):
+        return False
+    return stub_func
+
+
+def test_sanity_check_passes_sanity(always_true_algorithm_stub):
+    name = "always_true"
+    test_sanity_map = {name: always_true_algorithm_stub}
+    data = "placeholder"
+    assert sanity_check(name, data, sanity_map=test_sanity_map) == True
+
+
+def test_sanity_check_fails_sanity(always_false_algorithm_stub):
+    name = "always_false"
+    test_sanity_map = {name: always_false_algorithm_stub}
+    data = "placeholder"
+    assert sanity_check(name, data, sanity_map=test_sanity_map) == False
+
+
+def test_sanity_check_algorithm_name_doesnt_exist():
+    name = "nope"
+    test_sanity_map = {"not_real": "me_either"}
+    data = "placeholder"
+    with pytest.raises(ValueError) as e_info:
+        sanity_check(name, data, sanity_map=test_sanity_map)
